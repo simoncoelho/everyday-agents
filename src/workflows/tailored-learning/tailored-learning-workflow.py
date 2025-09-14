0cb4dd8e816d978214c5262e5f2b6fbd77ef0542
Simon <coelhosimon@hotmail.com>
2025-09-14 14:45:25 -0600

    updates

--- src/workflows/tailored-learning/tailored-learning-workflow.py
+++ src/workflows/tailored-learning/tailored-learning-workflow.py
@@ -1,6 +1,7 @@
 from agents import Agent, HostedMCPTool, Runner
 from typing import List
 from .agent import tailored_lesson_agent
+import asyncio
 
 
 class TailoredLearningWorkflow:
@@ -8,11 +9,20 @@ class TailoredLearningWorkflow:
                 number_of_pages_low: int = 3,
                 number_of_pages_high: int = 7,
                 number_of_documents: int = 4):
-        self.page_counts = [number_of_pages_low, number_of_pages_high],
-        self.document_count = number_of_documents,
-        self.agent = tailored_lesson_agent.get_learning_agent(page_counts)
+        self.page_counts = [number_of_pages_low, number_of_pages_high]
+        self.document_count = number_of_documents
+        self.agent = tailored_lesson_agent.get_learning_agent(self.page_counts)
 
-    def run():
-        range(self.document_count)
+    async def run(self):
+        for _ in range(self.document_count):
+            print("Beginning run...")
+            result = await Runner.run(self.agent)
+            print("Agent completed first pdf.")
+            print(result.final_output)
+        print("Completed all runs.")
 
-        Runner.run(self.agent, )
+
+
+if __name__ == "__main__":
+    workflow = TailoredLearningWorkflow
+    asyncio.run(workflow.run())
